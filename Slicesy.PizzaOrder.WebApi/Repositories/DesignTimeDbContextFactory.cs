58ef8fe7b688bd8e3551da627912e6ee2d3e46bd
Haseoo <dawid_szymkiewicz@hotmail.com>
2020-02-19 13:32:21 +0100

    Add Design time dbcontext factory

---
+++ Slicesy.PizzaOrder.WebApi/Repositories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Slicesy.PizzaOrder.WebApi.Repositories
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<RepositoryContext>();
+            var connectionString = configuration["databaseConnection:connectionString"];
+            builder.UseNpgsql(connectionString);
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
